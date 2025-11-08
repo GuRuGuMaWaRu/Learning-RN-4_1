6784e269ed798b55314cf3a84ba372e056c46479
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-11-08 09:31:27 +0200

    Use dynamic data for Favorites screen

--- app/(tabs)/favorite.tsx
+++ app/(tabs)/favorite.tsx
@@ -1,16 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+import { useFocusEffect } from 'expo-router';
+import { useCallback } from 'react';
 import { ResponsiveGrid } from 'react-native-flexible-grid';
 
 import Container from '@/components/Container';
 import Card from '@/components/favorite/Card';
 import Header from '@/components/Header';
-import { FAVORITES } from '@/core/constants';
+import LoadingIndicator from '@/components/LoadingIndicator';
+import { client } from '@/core/api/client';
 
 const Favorite = () => {
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ['favorites'],
+    queryFn: async () => {
+      const response = await client.get('/favorites');
+      return response.data.favorites;
+    },
+  });
+
+  useFocusEffect(
+    useCallback(() => {
+      refetch();
+    }, [refetch])
+  );
+
+  if (isLoading || !data) {
+    return <LoadingIndicator />;
+  }
+
   return (
     <Container>
       <Header title="Favorite" />
       <ResponsiveGrid
-        data={FAVORITES as Property[]}
+        data={data as Property[]}
         renderItem={({ item }) => <Card property={item} />}
         keyExtractor={(item: Property) => item.id}
         maxItemsPerColumn={2}
