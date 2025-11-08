bc4187231deb4b3c0540b1b219f6bb07777ef19d
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-11-08 09:31:07 +0200

    Use loading indicator for Properties List screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,25 @@
-import { FlatList, View } from 'react-native';
+import { FlatList } from 'react-native';
 
 import Container from '@/components/Container';
 import Card from '@/components/home/Card';
 import Discovery from '@/components/home/Discovery';
 import MainHeader from '@/components/home/MainHeader';
-import Text from '@/components/Text';
+import LoadingIndicator from '@/components/LoadingIndicator';
 import { client } from '@/core/api/client';
-import { PROPERTIES } from '@/core/constants';
 import { useQuery } from '@tanstack/react-query';
 
 export default function Home() {
   const { data, isLoading } = useQuery({
-    queryKey: ['properties-list'],
+    queryKey: ['properties'],
     queryFn: async () => {
       const { data } = await client.get('/properties');
       return data.properties;
     },
   });
 
-  if (isLoading)
-    return (
-      <Container>
-        <View className="flex flex-row items-center justify-center">
-          <Text>Loading...</Text>
-        </View>
-      </Container>
-    );
+  if (!data || isLoading) {
+    return <LoadingIndicator />;
+  }
 
   return (
     <Container>
@@ -35,7 +29,7 @@ export default function Home() {
         renderItem={({ item }) => <Card property={item} />}
         keyExtractor={(item) => item.id}
         showsVerticalScrollIndicator={false}
-        ListHeaderComponent={() => <Discovery properties={PROPERTIES} />}
+        ListHeaderComponent={() => <Discovery properties={data.reverse()} />}
       />
     </Container>
   );
